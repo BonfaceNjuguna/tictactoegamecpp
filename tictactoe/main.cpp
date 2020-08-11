da372c55343d1a8c91ad0c74dcae988df96aa0b3
Bonface Njuguna <bonface.njuguna@learninglions.org>
2020-08-11 23:06:12 +0300

    added checkwin command

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
 
 int checkwin();
-void board();
+//void board();
 
 int main()
 {
@@ -70,4 +70,39 @@ int main()
     cin.ignore();
     cin.get();
     return 0;
+}
+
+int checkwin()
+{
+    if (square[1] == square[2] && square[2] == square[3])
+
+        return 1;
+    else if (square[4] == square[5] && square[5] == square[6])
+
+        return 1;
+    else if (square[7] == square[8] && square[8] == square[9])
+
+        return 1;
+    else if (square[1] == square[4] && square[4] == square[7])
+
+        return 1;
+    else if (square[2] == square[5] && square[5] == square[8])
+
+        return 1;
+    else if (square[3] == square[6] && square[6] == square[9])
+
+        return 1;
+    else if (square[1] == square[5] && square[5] == square[9])
+
+        return 1;
+    else if (square[3] == square[5] && square[5] == square[7])
+
+        return 1;
+    else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
+        && square[4] != '4' && square[5] != '5' && square[6] != '6'
+        && square[7] != '7' && square[8] != '8' && square[9] != '9')
+
+        return 0;
+    else
+        return -1;
 }
